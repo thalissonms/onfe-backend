6cd84d2a66d7dcb9346cce030eb0fa3860dbd542
Thalisson <deco_kiler@hotmail.com>
2019-10-29 15:17:49 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const routes = require('./routes')
 const server = express()
 
-
 mongoose.connect('mongodb+srv://testdata:admin@cluster0-1pdfy.mongodb.net/loja?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -16,7 +15,4 @@ server.use(express.json())
 server.use(routes)
 
 
-var port = process.env.PORT || 3000;
-server.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
-});
+server.listen(process.env.PORT || 3333)
