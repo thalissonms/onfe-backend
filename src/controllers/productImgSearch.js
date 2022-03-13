fc71b3996849a03fc5599893de52ba83b632232d
Thalisson Silva <54037942+thalissonms@users.noreply.github.com>
2022-03-13 03:10:18 -0300

    Update productImgSearch.js

--- src/controllers/productImgSearch.js
+++ src/controllers/productImgSearch.js
@@ -5,8 +5,8 @@ module.exports = {
     async store(req, res) {
         const { p } = req.params
 const resp = await customSearch.cse.list({
-    auth:'AIzaSyBn50-oYDqD3ioQ9zj4BkwmTFv9I2BusqM',
-    cx:'012358337667130757373:lfqh9lhbutg',
+    auth:'GOOGLE API',
+    cx:'GOOGLE API',
     q: p,
     searchType: 'image',
     num:4
